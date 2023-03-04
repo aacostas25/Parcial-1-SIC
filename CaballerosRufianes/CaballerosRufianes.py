b06773a51eef89ef20f37fa9c14c3e7307469d1a
aacostas25 <aacostas@unal.edu.co>
2023-03-04 17:20:22 -0500

    Finalizado

--- CaballerosRufianes/CaballerosRufianes.py
+++ CaballerosRufianes/CaballerosRufianes.py
@@ -12,21 +12,27 @@ CRufian = Symbol("C es un Rufian")
 # Primera prueba
 # A dice "Soy ambos: Un Caballero y un Rufian."
 knowledge0 = And(
-	# TODO
+	Biconditional(ACaballero,Not(ARufian)),
+    Biconditional(ACaballero,And(ACaballero,ARufian))
 )
 
 # Puzzle 1
 # A dice "Somos ambos rufianes."
 # B no dice nada.
 knowledge1 = And(
-	# TODO
+	Biconditional(ACaballero,Not(ARufian)),
+    Biconditional(BCaballero,Not(BRufian)),
+    Biconditional(ACaballero,And(ARufian,BRufian))
 )
 
 # Puzzle 2
 # A dice "Somos del mismo tipo."
 # B dice "Somos de tipo distinto."
 knowledge2 = And(
-	# TODO
+	Biconditional(ACaballero,Not(ARufian)),
+    Biconditional(BCaballero,Not(BRufian)),
+    Biconditional(ACaballero,Or(And(ACaballero,BCaballero),And(ARufian,BRufian))),
+    Biconditional(BCaballero,Not(Or(And(ACaballero,BCaballero),And(ARufian,BRufian))))
 )
 
 # Puzzle 3
@@ -35,8 +41,15 @@ knowledge2 = And(
 # B dice "C es un Rufian."
 # C dice "A es un Caballero."
 knowledge3 = And(
-	# TODO  
+	Biconditional(ACaballero,Not(ARufian)),
+    Biconditional(BCaballero,Not(BRufian)),
+    Biconditional(CCaballero,Not(CRufian)),
 
+    Biconditional(CCaballero,ACaballero), # C dice "A es un Caballero."
+    Biconditional(BCaballero,CRufian),
+    #para nosotros esto es lo necesari  Implication(ARufian,BRufian)
+    # pero para que el algoritmo piense mas por el mismo
+    Implication(BCaballero,Biconditional(ACaballero,ARufian))
 )
 
 
