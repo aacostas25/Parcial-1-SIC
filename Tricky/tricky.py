6c4431e336be67a902a6a70ddb12f1f243764511
aacostas25 <aacostas@unal.edu.co>
2023-03-06 23:47:42 -0500

    Optimizado el X

--- Tricky/tricky.py
+++ Tricky/tricky.py
@@ -20,7 +20,6 @@ def initial_state():
 
 
 def player(board):
-    
     #Retorna quien es el jugador que debe jugar.
    
     #Siempre empieza la X
@@ -84,44 +83,44 @@ def terminal(board):
         return True
     if utility(board) ==1 or utility(board)==-1:
        return True
+    else:
+       return False
     """
     Retorna True si el juego se acabó y False si continua.
     """
 def utility(board):
+    t=0
     #Para X
     #Comprobar lineas horizontales
     for i in range(0,3):
       if board[i][0] == X and board[i][1] == X and board[i][2] == X:
-        if player(board) == X:
-          return 1
+        return 1
     #Comprobar lineas verticales
     for j in range(0,3):
       if board[0][j] == X and board[1][j] == X and board[2][j] == X:
-        if player(board) == X:
-          return 1
+        return 1
     #Comprobar diagonales
     if (board[0][0] == X and board[1][1] == X and board[2][2] == X) or (board[2][0] == X and board[1][1] == X and board[0][2] == X):
-      if player(board) == X:
         return 1
 
     #Para O
     #Comprobar lineas horizontales
     for i in range(0,3):
       if board[i][0] == O and board[i][1] == O and board[i][2] == O:
-        if player(board) == O:
-          return -1
+        return -1
     #Comprobar lineas verticales
     for j in range(0,3):
       if board[0][j] == O and board[1][j] == O and board[2][j] == O:
-        if player(board) == O:
-          return -1
+        return -1
     #Comprobar diagonales
     if (board[0][0] == O and board[1][1] == O and board[2][2] == O) or (board[2][0] == O and board[1][1] == O and board[0][2] == O):
-      if player(board) == O:
         return -1
 
-    # Como el return automaticaente termina la lectura del código si se llega a esta linea es porque nadie ganó en el estado terminal
-    return 0
+   
+   # Como el return automaticaente termina la lectura del código si se llega a esta linea es porque nadie ganó en el estado terminal
+    if t ==0:
+       return 0
+    #return 0
 
     #Retorna 1 si X ganó el juego, -1 si O ganó, y 0 si empatan.
     
@@ -145,33 +144,24 @@ def max_value(board):
         v = max(v,min_value(result(board,action)))
     return v
 
-
 def minimax(board):
-    #Ya no hay nada que optimizar
-    if terminal(board):
-       return None
-    elif player(board) == X:
-    #else:
-       v = math.inf
-       mejorJugada = ()
-       for action in actions(board):
-        puntaje = max_value(result(board,action))
-        if puntaje < v:
+   if terminal(board):
+      return None
+   elif player(board)==X:
+      v = -math.inf
+      mejorJugada = ()
+      for action in actions(board):
+         puntaje = max_value(result(board,action))
+         if puntaje > v:
             v = puntaje
             mejorJugada = action
-        return mejorJugada
-    elif player(board) == O:
-       v = -math.inf
-       mejorJugada = ()
-       for action in actions(board):
-        puntaje = min_value(result(board,action))
-        if v < puntaje:
-            puntaje = v
+      return mejorJugada
+   elif player(board)==O:
+      v = math.inf
+      mejorJugada = ()
+      for action in actions(board):
+         puntaje = min_value(result(board,action))
+         if v > puntaje:
+            v = puntaje
             mejorJugada = action
-        return mejorJugada
-    
-    #Retorna la acción que optimiza la elección del jugador.
-
-
-
-
+      return mejorJugada
